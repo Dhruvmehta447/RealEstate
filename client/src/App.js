4627a4565140c6dc20b0cd00554b85ad3aee5edd
dhruvmehta <purvismac@Purviss-MacBook-Air.local>
2025-12-20 01:56:10 +0530

    Connected frontend to live backend

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import AdminPanel from './pages/AdminPanel';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://realestate-l4le.onrender.com';
 
 function App() {
   return (
